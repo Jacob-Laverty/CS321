cfda57f15f96fe119a7f1d1762d7b918f2f19139
Jacob-Laverty <jacob.laverty@gmail.com>
2014-11-20 16:28:38 -0500

    removed main class and added RegulateTemp class

---
+++ src/main/java/com/cs/gmu/project321/RegulateTemp.java
@@ -0,0 +1,8 @@
+package com.cs.gmu.project321;
+
+/**
+ * Created by Jacob on 11/20/14.
+ */
+public class RegulateTemp {
+
+}
---
+++ src/test/java/com/cs/gmu/project321/RegulateTempTest.java
@@ -0,0 +1,9 @@
+package com.cs.gmu.project321;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jacob on 11/20/14.
+ */
+public class RegulateTempTest {
+}
