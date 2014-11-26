2b3d1f7dc8092f633c2198c3f4a41a0951b0c2fc
Jacob-Laverty <jacob.laverty@gmail.com>
2014-11-25 22:31:14 -0500

    Added TempTest class

---
+++ src/test/java/com/cs/gmu/project321/TemperatureTest.java
@@ -0,0 +1,18 @@
+package com.cs.gmu.project321;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jacob on 11/25/14.
+ */
+public class TemperatureTest {
+  private static Temperature temp = new Temperature();
+
+  @Test
+  public void setTempTest() {
+    this.temp.set(50);
+
+    assertEquals(50l, (long)this.temp.get());
+  }
+}
